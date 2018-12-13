c5751d021f06135abd1dd4032a56ea9913b1372a
Prashant Shukla <prashantshukla@qainfotech.com>
2018-12-13 13:09:24 +0530

    removed anicillary files

--- _site/README.md
+++
@@ -1,1 +0,0 @@
-# prashantshukla-qa.github.io
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
-var pug         = require('pug');
+var pug         = require('gulp-pug');
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
